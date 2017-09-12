f9d52c3830ef6b49a2a50536ce4412b7753ac371
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-09-12 15:41:03 -0600

    Modified tsconfig file, closure files and added classes and inheritance TypeScript files

--- closure_intro.js
+++ closure_intro.js
@@ -1,12 +1,13 @@
 // functions have access to any public variables in the outer scope
-function nameFunction(name) {
-    var n = name;
-    function printName() {
-        console.log(n);
-    }
-    printName();
-}
-nameFunction('Steven');
+// function nameFunction(name: string) : void {
+// 	var n : string = name;
+// 	function printName() {
+// 		console.log(n);
+// 	}
+// 	printName();
+// }
+// nameFunction('Steven');
+// * * * * * * * * * * * * * * * *
 // The inner functions maintain access to the outer scope even after the values are returned!
 // function nameFunction(name: string) {
 // 	var n : string = name;
@@ -16,7 +17,7 @@ nameFunction('Steven');
 // }
 // var nameAgain = nameFunction('Steven');
 // nameAgain();
-// * * *
+// * * * * * * * * * * * * * * * * 
 // function lineup() {
 // 	var nowBatting : number = 1;
 // 	return {
